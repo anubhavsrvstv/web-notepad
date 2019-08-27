25701425a0e1b800c12d437d45a07c665e4cdfec
anubhavsrvstv <54494984+anubhavsrvstv@users.noreply.github.com>
2019-08-27 11:44:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended:true}));
 const port = process.env.PORT||5555;
 const mongoose = require('mongoose');
-mongoose.connect( 'mongodb+srv://anubhav:1234567890@cluster0-7pma8.gcp.mongodb.net/test?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/test',function(err,db){
+mongoose.connect( 'mongodb+srv://anubhav:1234567890@cluster0-7pma8.gcp.mongodb.net/test?retryWrites=true&w=majority' || 'mongodb://193.17.124.1/32',function(err,db){
 
    if (err){
         console.log("error",err);
