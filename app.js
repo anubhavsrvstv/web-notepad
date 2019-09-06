9cabde0e4577ae6234bd8aab47bd65c26eb78fcb
anubhavsrvstv <54494984+anubhavsrvstv@users.noreply.github.com>
2019-09-06 18:34:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var cors = require('cors')
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended:true}));
-const port = process.env.PORT;
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cors());
+
+const port = process.env.PORT || 5555;
 const mongoose = require('mongoose');
 var mydb;
+var uri = 'mongodb://localhost:27017/test';
 
-mongoose.connect( 'mongodb+srv://anubhav:1234567890@cluster0-7pma8.gcp.mongodb.net/test?retryWrites=true&w=majority',function(err,db){
 
-   if (err){
-        console.log("error",err);
+mongoose.connect(uri,{useNewUrlParser:true},function(err,db){
+    if (err) {
+        console.log("error", err);
     }
-    else{
-         mydb =db;
+    else {
+        console.log("connected")
+        mydb = db;
     }
-} );
+});
 
 
-app.use(express.static('.')); 
+app.use(express.static('.'));
 
 app.get('/', function (req, res) {
     res.send('WEB NOTEPAD!');
@@ -26,34 +31,34 @@ app.get('/', function (req, res) {
 
 app.get('/register', function (req, res) {
     console.log("form print")
-    res.sendFile(__dirname + '/note.html');
+    res.sendFile(__dirname + '/notepad.html');
 });
 
 app.post('/form_data', function (req, res) {
     console.log("abhi");
-    console.log("req",req.body);
-    mydb.collection('formTest').insertOne(req.body,function(err,data){
+    console.log("req", req.body);
+    mydb.collection('formTest').insertOne(req.body, function (err, data) {
         if (err) throw err;
-        console.log('data',data);
+        console.log('data', data);
         // res.sendFile(__dirname + '/note.html',data);
-        res.send({success:true,mydata:data});
+        res.send({ success: true, mydata: data });
     })
 });
 
 // /app.get('/home', function (req, res) {
-    // var myobj = {name: "ram",age:26};
-    // mydb.collection('hello').insertOne(myobj,function(err,data){
-        // if (err) throw err;
-        // res.send({success:true,mydata:data});
+// var myobj = {name: "ram",age:26};
+// mydb.collection('hello').insertOne(myobj,function(err,data){
+// if (err) throw err;
+// res.send({success:true,mydata:data});
 
-    // })
-    
+// })
+
 // });
 
 app.get('/getData', function (req, res) {
     mydb.collection('formTest').find({}).toArray(function (err, response) {
         if (err) throw err;
-        console.log("response", response);
+        // console.log("response", response);S
 
         res.setHeader('Content-Type', 'application/json');
         res.json({ data: response });
@@ -74,6 +79,12 @@ app.get('/updateData', function (req, res) {
         res.send({ success: "true" });
 
 
+
+
+
+
+
+
     })
 
 });
@@ -83,16 +94,44 @@ app.get('/updateData', function (req, res) {
 
 
 
+app.get('/deleteName',function(req,res){
+    console.log("mydb",mydb.name);
+    mydb.collection('formTest').find({},function(err,response){
+        if(err) throw err;
+        else{
+            console.log("res",response);
+            res.send({status:"success"});
+        }
+    })
+})
+
+app.post('/deleteData', function (req, res) {
+    console.log("idddddddddd", req.headers.id);
+    // mydb.collection("formTest").deleteOne({ _id: req.headers.id }, function (err, obj) {
+    //     if (err) throw err;
+    //     console.log("deleteed Data",obj);
+    //     res.send({ success: "deleted" });
 
 
-app.delete('/deleteData', function (req, res) {
-    mydb.collection("formTest").deleteOne({ name: "Anubhav" }, function (err, obj) {
+    // });
+
+    mydb.collection('formTest').deleteOne({ _id: "5d7235f8249f531d80641ba0" },function (err, response) {
         if (err) throw err;
-        // console.log(obj.result.n + '/note.html');
-        res.send({ success: "true" });
+        else {
+            console.log("data",data);
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ data: data });
+        }
+    })
+    // mydb.collection('formTest').deleteO({_id:req.headers.id}).toArray(function (err, response) {
+    //     if (err) throw err;
+    //     console.log("response", response);
+
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.json({ data: response });
 
 
-    });
+    // });
 
 });
 
@@ -100,7 +139,7 @@ app.delete('/deleteData', function (req, res) {
 
 
 var server = app.listen(port, function () {
-    console.log("server started on port....."+port);
- });
+    console.log("server started on port....." + port);
+});
 
 
---
+++ notepadcontroller.js
@@ -0,0 +1,65 @@
+var app = angular.module('myApp', []);
+app.controller('notepadCtrl', function($scope,$http, $window) {
+
+  $scope.getData = function() {
+    // console.log()
+    $http({
+        method : "GET",
+          url : 'http://localhost:5555/getdata'
+      }).then(function mySuccess(response) {
+        $scope.data = response.data.data;
+        console.log(response);
+        
+
+      }, function myError(err) {
+          console.log(err);
+        // $scope.myWelcome = response.statusText;
+      });
+  };
+
+
+  $scope.dataUpdate = function () {
+    console.log("response");
+    // $http({
+    //     method : "Update",
+    //       url : 'http://localhost:5555/updatedata'
+    //   }).then(function mySuccess(response) {
+    //     $scope.data = response.data.data;
+    //     console.log(response);
+  
+    //   }, function myError(err) {
+    //       console.log(err);
+    //     // $scope.myWelcome = response.statusText;
+    //   });
+  };
+  
+  // this  should be same 
+  $scope.removeData = function (id) {
+    console.log("id",id);
+    $http({
+        method : "POST",
+          url : 'http://localhost:5555/deleteData',
+          headers: { 'id':id,'Content-Type': 'application/json', 'Cache-Control': 'no-cache'}
+      }).then(function mySuccess(response) {
+        $scope.data = response.data.data;
+        console.log(response);
+        $window.location.reload();
+        
+  
+      }, function myError(err) {
+          console.log(err)
+        // $scope.myWelcome = response.statusText;
+      
+  
+    
+  
+        
+      });
+  };
+
+
+
+
+});
+
+
